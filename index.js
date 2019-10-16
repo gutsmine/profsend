deb9299589d19570a123ea21c18a2e58a52e8e11
gutsmine-tech <42506694+gutsmine-tech@users.noreply.github.com>
2019-10-16 09:30:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require("http");
 var fs = require('fs');
 
+
+var top = fs.readFileSync('./data/ top. html', 'utf-8');
+var about = fs.readFileSync('./data/ about. html', 'utf-8');
+var update = fs.readFileSync('./date/ 13update. html', 'utf-8');
+
 function getType(_url) {
 	var types = {
 		".html": "text/html",
@@ -23,13 +28,13 @@ var target = '';
 switch (req.url){
    case '/':
    case '/top.html':			 
-   case '/top': target = './data/top.html';
+   case '/top': target = top;
     break;
    case '/about':
-   case '/about.html': target = './data/about.html';
+   case '/about.html': target = about;
    　break;
    case '/13update':
-   case '/13update.html': target = './data/13update.html';
+   case '/13update.html': target = update;
    　break;			 
    default:
    　res.writeHead(404,{'Content-Type':'text/plain'});
@@ -39,7 +44,7 @@ switch (req.url){
 	
 
  fs.readFile(target,'utf-8',function(err,data){
-   res.writeHead(200,{'Content-Type':'text/html'});
+   res.writeHead(200, {"Content-Type": getType(url)});
    res.write(data);
    res.end();
  });
