d58521ff0df8f138947dc22d7c23d94cf8a94464
gutsmine-tech <42506694+gutsmine-tech@users.noreply.github.com>
2019-10-16 09:37:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var http = require("http");
 var fs = require('fs');
 
-
-var top = fs.readFileSync('./data/ top. html', 'utf-8');
-var about = fs.readFileSync('./data/ about. html', 'utf-8');
-var update = fs.readFileSync('./date/ 13update. html', 'utf-8');
-
 function getType(_url) {
 	var types = {
 		".html": "text/html",
@@ -24,31 +19,24 @@ function getType(_url) {
 }
 
 var server = http.createServer(function (req, res) {
-var target = '';	
-switch (req.url){
-   case '/':
-   case '/top.html':			 
-   case '/top': 
-    target = top;
-    break;
-   case '/about':
-   case '/about.html': 
-    target = about;
-   　break;
-   case '/13update':
-   case '/13update.html': 
-    target = update;
-   　break;			 
-   default:
-   　res.writeHead(404,{'Content-Type':'text/plain'});
-   　res.end('bad request');
-   return;
- }
-	
+	var url = "data" + (req.url.endsWith("/") ? req.url + "top.html" : req.url);
+	if (fs.existsSync(url)) {
+		fs.readFile(url, (err, data) => {
+			if (!err) {
+				res.writeHead(200, {"Content-Type": getType(url)});
+				res.end(data);
+			} else {
+				res.statusCode = 500;
+				res.end();
+			}
+		});
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}
+});
 
- fs.readFile(target,'utf-8',function(err,data){
-   res.writeHead(200, {"Content-Type": getType(url)});
-   res.write(data);
-   res.end();
- });
-server.listen(1234);
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+;
+});
