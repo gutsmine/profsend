d2a9d7be0172ea790bc9f1bf32d0f0a5a1d806a5
guts <mine>
2019-10-25 19:30:01 +0900

    FIX js

--- index.js
+++ index.js
@@ -2,19 +2,46 @@ var express = require('express')
 var app = express();
 var session = require('express-session');
 var i18n = require("i18n");
+var router = require("express").Router();
 
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 5000));
 app.use("/", express.static('./data/public'));
 
+router.get("/", (req, res) => {
+    res.render("./public/top.ejs");
+});
+router.get("/top.html", (req, res) => {
+    res.render("./top.ejs");
+});
+router.get("/about.html", (req, res) => {
+    res.render("./about.ejs");
+});
+router.get("/13update.html", (req, res) => {
+    res.render("./13update.ejs");
+});
+
+i18n.configure({
+  // 利用するlocalesを設定。これが辞書ファイルとひも付きます
+  locales: ['ja', 'en'],
+  defaultLocale: 'ja',
+  // 辞書ファイルのありかを指定
+  directory: __dirname + "/locales",
+  // オブジェクトを利用したい場合はtrue
+  objectNotation: true
+});
+
 app.use(i18n.init);
-// manualでi18nセッション管理できるように設定しておきます
 app.use(function (req, res, next) {
   if (req.session.locale) {
     i18n.setLocale(req, req.session.locale);
   }
   next();
 });
+
+
+module.exports = router;
+
 app.listen(app.get('port'), function () {
 console.log("Server started.");
 });
