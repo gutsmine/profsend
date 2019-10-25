b812a9050098da1271274ad2474596724c3e60e8
guts <mine>
2019-10-25 18:27:14 +0900

    ADD CI data

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 var express = require('express')
 var app = express();
+var session = require('express-session');
+var i18n = require("i18n");
 
+app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 5000));
 app.use("/", express.static('./data/public'));
 
-app.get('/', function(req, res) {
-    res.sendFile('./data/public/top.html', { root: __dirname });
+app.use(i18n.init);
+// manual‚Åi18nƒZƒbƒVƒ‡ƒ“ŠÇ—‚Å‚«‚é‚æ‚¤‚Éİ’è‚µ‚Ä‚¨‚«‚Ü‚·
+app.use(function (req, res, next) {
+  if (req.session.locale) {
+    i18n.setLocale(req, req.session.locale);
+  }
+  next();
 });
-
-app.get('/top.html', function(req, res) {
-    res.sendFile('./data/public/top.html', { root: __dirname });
-});
-
-app.get('/about.html', function(req, res) {
-    res.sendFile('./data/public/about.html', { root: __dirname });
-});
-
-app.get('/13update.html', function(req, res) {
-    res.sendFile('./data/public/13pudate.html', { root: __dirname });
-});
-
 app.listen(app.get('port'))
