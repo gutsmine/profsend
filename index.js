1a902c7a134b2f9b311f8be19f668c45caf714c9
guts <mine>
2019-10-26 09:06:49 +0900

    FIX js

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ var i18n = require("i18n");
 app.set("view engine", "ejs");
 app.use("/", express.static('./data/public'));
 app.use("/", require("./router.js"));
+
+var router = require("express").Router();
+app.use(function (req, res, next) {
+router.get("/", (req, res) => {
+    res.render("./data/public/top.ejs");
+});
+router.get("/top.html", (req, res) => {
+    res.render("./data/public/top.ejs");
+});
+router.get("/about.html", (req, res) => {
+    res.render("./data/public/about.ejs");
+});
+router.get("/13update.html", (req, res) => {
+    res.render("./data/public/13update.ejs");
+});
+});
+module.exports = router;
+
 i18n.configure({
   // 利用するlocalesを設定。これが辞書ファイルとひも付きます
   locales: ['ja', 'en'],
