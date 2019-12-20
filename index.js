7c445227dd8df8d95a259f8c50322d94b0cbe22e
gutsmine <mine>
2019-12-20 19:56:42 +0900

    FIX index

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.use("/", express.static('./data/public'));
 
 app.get('/', function(req, res) {
-    res.sendFile('./data/public/top.html', { root: __dirname });
+    res.sendFile('top.html', { root: __dirname + '/data/public/'});
 });
 
 app.get('/top.html', function(req, res) {
-    res.sendFile('./data/public/top.html', { root: __dirname });
+    res.sendFile('top.html', { root: __dirname + '/data/public/'});
 });
 
 app.get('/about.html', function(req, res) {
-    res.sendFile('./data/public/about.html', { root: __dirname });
+    res.sendFile('about.html', { root: __dirname + '/data/public/'});
 });
 
 app.get('/13update.html', function(req, res) {
-    res.sendFile('./data/public/13pudate.html', { root: __dirname });
+    res.sendFile('13pudate.html', { root: __dirname + '/data/public/'});
 });
 
 app.listen(app.get('port'));
