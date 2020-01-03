5b51f07689b3deade724177ca022fe9689793069
gutsmine <mine>
2020-01-03 10:55:59 +0900

    FIX

--- app.test.js
+++ app.test.js
@@ -1,8 +1,8 @@
-const request = require('supertest');
+﻿const request = require('supertest');
 const app = require('./app')
 
-describe('Test the root path', () => {
-    test('It should response the GET method', () => {
+describe('アクセステスト', () => {
+    test('ステータスコード 200', () => {
         return request(app).get("/").then(response => {
             expect(response.statusCode).toBe(200)
         })
