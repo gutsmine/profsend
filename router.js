31f470f17b552b88b7960b910b9aa3557a75bcdc
gumieProf <mine>
2020-06-01 18:20:24 +0900

    FIX router UPDATE views

--- router.js
+++ router.js
@@ -1,38 +1,38 @@
 var router = require("express").Router();
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.render("./public/top.ejs");
 });
 
-router.get("/top.html", function(req, res) {
-  res.render("./public/top.ejs", data);
+router.get("/top.html", function (req, res) {
+  res.render("./public/top.ejs");
 });
 
-router.get("/about.html", function(req, res) {
-  res.render("./public/about.ejs", data);
+router.get("/about.html", function (req, res) {
+  res.render("./public/about.ejs");
 });
 
-router.get("/top", function(req, res) {
-  res.render("./public/top.ejs", data);
+router.get("/top", function (req, res) {
+  res.render("./public/top.ejs");
 });
 
-router.get("/about", function(req, res) {
-  res.render("./public/about.ejs", data);
+router.get("/about", function (req, res) {
+  res.render("./public/about.ejs");
 });
 
-router.get("/13update", function(req, res) {
-  res.render("./public/13update.ejs", data);
+router.get("/13update", function (req, res) {
+  res.render("./public/13update.ejs");
 });
-router.get("/13update.html", function(req, res) {
-  res.render("./public/13update.ejs", data);
+router.get("/13update.html", function (req, res) {
+  res.render("./public/13update.ejs");
 });
-router.get("/top.pwa", function(req, res) {
+router.get("/top.pwa", function (req, res) {
   res.sendFile("top.pwa.html", { root: __dirname + "/views/public/" });
 });
-router.get("/13update.pwa", function(req, res) {
+router.get("/13update.pwa", function (req, res) {
   res.sendFile("13update.pwa.html", { root: __dirname + "/views/public/" });
 });
-router.get("/about.pwa", function(req, res) {
+router.get("/about.pwa", function (req, res) {
   res.sendFile("about.pwa.html", { root: __dirname + "/views/public/" });
 });
 
