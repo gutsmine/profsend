36416e3fa88a9e583def09a4ebe70f6fac7f7b6c
gumieProf <mine>
2020-08-12 18:00:58 +0900

    FIX app router package

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require("express");
+var helmet = require('helmet');
 var cookieParser = require("cookie-parser");
+var session = require('express-session')
+
 var app = express();
-var cookieParser = require("cookie-parser");
 app.use(cookieParser());
+app.use(session({
+	secret: 'Profsend is secure',
+	cookie: {
+	 secure: true,
+	 domain: "profsend.ga",
+	 sameSite: "lax" 
+ }
+}));
+app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
 app.set("port", process.env.PORT || 3000);
 app.use("/", express.static(__dirname + "/views/public"));
 app.use("/script", express.static(__dirname + "/views/public/script"));
--- router.js
+++ router.js
@@ -1,41 +1,45 @@
 var router = require("express").Router();
+var csrf = require('csurf');
 
-router.get("/", function (req, res) {
+var csrfProtection = csrf({ cookie: false });
+
+
+router.get("/", csrfProtection, function (req, res) {
   res.sendFile("top.html", { root: __dirname + "/views/public/" });
 });
 
-router.get("/top.html", function (req, res) {
+router.get("/top.html", csrfProtection, function (req, res) {
   res.sendFile("top.html", { root: __dirname + "/views/public/" });
 });
 
-router.get("/about.html", function (req, res) {
+router.get("/about.html", csrfProtection, function (req, res) {
   res.sendFile("about.html", { root: __dirname + "/views/public/" });
 });
 
-router.get("/top", function (req, res) {
+router.get("/top", csrfProtection, function (req, res) {
   res.sendFile("top.html", { root: __dirname + "/views/public/" });
 });
 
-router.get("/about", function (req, res) {
+router.get("/about", csrfProtection, function (req, res) {
   res.sendFile("about.html", { root: __dirname + "/views/public/" });
 });
 
-router.get("/13update", function (req, res) {
+router.get("/13update", csrfProtection, function (req, res) {
   res.sendFile("13update.html", { root: __dirname + "/views/public/" });
 });
-router.get("/13update.html", function (req, res) {
+router.get("/13update.html", csrfProtection, function (req, res) {
   res.sendFile("13update.html", { root: __dirname + "/views/public/" });
 });
-router.get("/top.pwa", function (req, res) {
+router.get("/top.pwa", csrfProtection, function (req, res) {
   res.sendFile("top.pwa.html", { root: __dirname + "/views/public/" });
 });
-router.get("/13update.pwa", function (req, res) {
+router.get("/13update.pwa", csrfProtection, function (req, res) {
   res.sendFile("13update.pwa.html", { root: __dirname + "/views/public/" });
 });
-router.get("/about.pwa", function (req, res) {
+router.get("/about.pwa", csrfProtection, function (req, res) {
   res.sendFile("about.pwa.html", { root: __dirname + "/views/public/" });
 });
-router.get("/pwa.top", function (req, res) {
+router.get("/pwa.top", csrfProtection, function (req, res) {
   res.sendFile("pwa.top.html", { root: __dirname + "/views/public/" });
 });
 
